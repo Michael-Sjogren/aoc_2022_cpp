6754fa0cc33a42a076a83341055d12893fe3cf6f
Michael <Michael-Sjogren>
2023-06-11 20:35:02 +0200

    wip

--- 5/main.cpp
+++ 5/main.cpp
@@ -1,17 +1,31 @@
 #include <fstream>
 #include <string>
-#include <stdio.h>
+#include <iostream>
 #include <string_view>
+
+
+void* operator new(std::size_t size) {
+    std::cout << size << " bytes allocated." << std::endl;
+    return malloc(size);
+}
+
+enum State {
+    GetBoxPositions,
+    ParseAndMoveBox,
+};
 
 int main()
 {
     std::ifstream file("input-test.txt");
     std::string line;
+    State state = State::GetBoxPositions;
+
     while (getline(file, line))
     {
-        
-        printf("%s\n",line.c_str());
+        auto line_view = std::string_view(line.c_str(), line.length());
+        std::cout << line_view << std::endl;
     }
     file.close();
     return 0;
 }
+
